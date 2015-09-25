03c96946d8e1be14a8548823761c419c58d76e67
andersrmr <andersrmr@aol.com>
2015-09-25 03:55:30 -0700

    Did modeling in prep for Friday practice presentation

--- source/build_models.py
+++ source/build_models.py
@@ -6,7 +6,7 @@ from sklearn.cross_validation import train_test_split
 from sklearn.cross_validation import cross_val_score
 import cPickle as pickle
 
-def clean_prep_before_model():
+def clean_prep_before_model(flag=True):
     '''
     INPUT: None
     OUTPUT: df
@@ -24,6 +24,22 @@ def clean_prep_before_model():
 
     # Remove rows where DURATION_td rounded to zero.
     df = df[df.DURATION_td != 0.000]
+
+    # Conditionally label NaN street features 'no street features'
+    if flag:
+        df.SND_FEACOD = df.SND_FEACOD.fillna('NO SND_FEACOD')
+        df.ST_CODE = df.ST_CODE.fillna('NO ST_CODE')
+        df.SEGMENT_TY = df.SEGMENT_TY.fillna('NO SEGMENT_TY')
+        df.DIVIDED_CO = df.DIVIDED_CO.fillna('NO DIVIDED_CO')
+        df.VEHICLE_US = df.VEHICLE_US.fillna('NO VEHICLE_US')
+
+    # Create another feature, number of potholes initiated on that day
+    # ***Later, move this to a function in create_features.py***
+    df_number_potholes = pd.DataFrame(df.groupby('INITDT_dt')['OBJECTID'].\
+        count()).reset_index()
+    df_number_potholes.rename(columns={'OBJECTID': 'Number_potholes'},\
+        inplace=True)
+    df = pd.merge(df, df_number_potholes, how='left', on='INITDT_dt')
 
     # Keep only the columns for modeling
     df = df.ix[:, ['neighborhood_label','INIT_Quarter','INIT_month','days_end_FY',\
@@ -43,8 +59,23 @@ def clean_prep_before_model():
         'Seattle_dist','latitude','longitude','Median_Value','Margin_of_Error']],\
         df.neighborhood_label.astype('category'),df.SND_FEACOD.astype('category'),\
         df.INIT_Quarter.astype('category'),df.ST_CODE.astype('category'),\
-        df.SEGMENT_TY.astype('category'),df.DIVIDED_CO.astype('category'),\
-        df.INIT_month.astype('int')], axis=1)
+        df.SEGMENT_TY.astype('category'),\
+        df.DIVIDED_CO.astype('category')], axis=1)
+
+    return df
+
+def define_target_vars(df):
+    '''
+    INPUT: df
+    OUTPUT: df
+    Define different target variables based on pothole repair time
+    '''
+    df['b_long_repair'] = df.DURATION_td > 3
+    df['b_long_repair'] = df['b_long_repair'].apply(lambda x: 1 if x == True else 0) 
+    
+    df['b_DURATION_td_95'] = df.DURATION_td < df.DURATION_td.quantile(.95)
+    df['b_DURATION_td_IQuart'] = (df.DURATION_td < df.DURATION_td.quantile(.75))\
+        & (df.DURATION_td > df.DURATION_td.quantile(.25))
 
     return df
 
